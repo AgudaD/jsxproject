5dbd1e1f49ec33cab938271b32c77aa332a900fd
Aguda <agudaoluwanifemi@gmail.com>
2021-11-14 20:22:38 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import imageInSrc from "./imageInSrc.jpg"
+import './styles.css';
 
 function App() {
+  let name = "David";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div style={{border:"solid 1px black", maxWidth:"100vw" }}>
+
+        <h1 className="title red">{name}</h1> <br />
+
+        <img src={imageInSrc} alt ='myImage' /> <br />
+        <img src="/imageInPublic.jpg" alt="myimage" />
+
+        </div>
+
+        <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+        
     </div>
   );
 }
