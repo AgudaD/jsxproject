e48161069cc732adad75ee994386e82a75473a7d
Aguda <agudaoluwanifemi@gmail.com>
2021-11-16 11:53:17 +0100

    jsx changes

--- src/App.js
+++ src/App.js
@@ -12,15 +12,22 @@ function App() {
 
         <h1 className="title red">{name}</h1> <br />
 
-        <img src={imageInSrc} alt ='myImage' /> <br />
-        <img src="/imageInPublic.jpg" alt="myimage" />
+        <div className="img-set">
+          <img className="img-style" src={imageInSrc} alt ='myImage' /> <br />
+          <img className="img-style" src="/imageInPublic.jpg" alt="myimage" />
+        </div>
+
+        <div className="vid-style">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/KSsG65TXo3Q" 
+          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+          encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+          
 
         </div>
 
-        <video width="320" height="240" controls>
-        <source src="myVideo.mp4" type="video/mp4" />
-        </video>
         
+    
     </div>
   );
 }
